ec3e51df97b58d88ee0fa9248d7525ed08658426
adamliu84 <adam.liu84@gmail.com>
2021-12-23 20:55:44 +0800

    Barebone and basic making TikTok Marketing API

---
+++ server/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const axios = require('axios');
+require('dotenv/config');
+const PORT = 3001
+
+app.use(cors());
+app.use(bodyParser.json());
+
+//Root routing
+app.get('/test', async (req, res) => {
+    axios.get(process.env.BASE_URL + "/reports/integrated/get/",
+        {
+            headers: {
+                'Access-Token': process.env.ACCESS_TOKEN
+            },
+            params: {
+                advertiser_id: process.env.ADVERTISER_ID,
+                dimensions: '["adgroup_id"]',
+                report_type: 'BASIC',
+                data_level: 'AUCTION_ADGROUP',
+                start_date: '2021-01-01',
+                end_date: '2021-12-31',
+            },
+        }).then(function (response) {            
+            res.json(response.data.data);
+        }).catch(function (error) {
+            console.error(error);
+        })
+});
+
+console.log("START BACKEND SERVER");
+app.listen(PORT);
--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
+import { useEffect, useState } from 'react'
 import logo from './logo.svg';
 import './App.css';
+const axios = require('axios');
 
 function App() {
+  const [testData, setTestData] = useState(null);
+
+  useEffect(() => {
+    axios.get("/test",
+    ).then(function (response) {
+      setTestData(JSON.stringify(response.data));
+    }).catch(function (error) {
+      console.error(error);
+    })
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <b>Sample data ↓</b> <br />
+        {testData}
       </header>
     </div>
   );
